cc4b92afca35fbdd9750bd93d60ec1b12dd57186
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2025-02-05 17:15:13 +0530

    change of dropdown

--- client/components/AddTaskModal.tsx
+++ client/components/AddTaskModal.tsx
@@ -11,14 +11,11 @@ import { useTaskStore } from "@/store/taskStore";
 import { useModalStore } from "@/store/modalStore";
 import { TaskPriority, TaskStatus } from "@/types/types";
 import { useDashboardStore } from "@/store/dashboardStore";
-import { 
-  Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle 
-} from "./ui/dialog";
-import { 
-  Select, SelectContent, SelectItem, SelectTrigger, SelectValue 
-} from "./ui/select";
+import {Dialog,DialogContent,DialogFooter,DialogHeader,DialogTitle,} from "./ui/dialog";
+import {Select,SelectContent,SelectItem,SelectTrigger,SelectValue,} from "./ui/select";
 
 const AddTaskModal = () => {
+
   const { newTask, updateTask, setNewTask, addTask } = useTaskStore();
   const { isAddModalOpen, setIsAddModalOpen } = useModalStore();
   const { user } = useDashboardStore();
@@ -30,37 +27,44 @@ const AddTaskModal = () => {
   };
 
   const handleAddTask = async () => {
-    try {
-      const url = newTask._id
-        ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/updatetask`
-        : `${process.env.NEXT_PUBLIC_BASE_URL}/api/addtask`;
 
-      const bodyData = {
-        ...newTask,
-        user: user?.email,
-      };
-
+    // if there is id present in task it will update that task
+    if (newTask._id) {
+      const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/updatetask`;
       const headers = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: newTask._id ? undefined : `Bearer ${user?.token}`,
         },
-        body: JSON.stringify(bodyData),
+        body: JSON.stringify({ ...newTask }),
       };
-
       const res = await fetch(url, headers);
       const data = await res.json();
 
-      if (newTask._id) {
-        updateTask(newTask);
-        toast({
-          title: "Task Updated",
-          variant: "default",
-          className: "bg-green-400 text-black",
-          duration: 2000,
-        });
-      } else {
+      updateTask(newTask);
+      toast({
+        title: "Task Updated",
+        variant: "default",
+        className: "bg-green-400 text-black",
+        duration: 2000,
+      });
+      setNewTask(EmptyTask);
+      setIsAddModalOpen(false);
+    } else {
+
+      // is there is no id present in task it will add new task to the list
+      try {
+        const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/addtask`;
+        const headers = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.token}`,
+          },
+          body: JSON.stringify({ ...newTask, user: user?.email }),
+        };
+        const res = await fetch(url, headers);
+        const data = await res.json();
         addTask(data.task);
         toast({
           title: "Task Added",
@@ -68,35 +72,40 @@ const AddTaskModal = () => {
           className: "bg-green-400 text-black",
           duration: 2000,
         });
+      } catch (error) {
+        console.error(error);
       }
 
       setNewTask(EmptyTask);
       setIsAddModalOpen(false);
-    } catch (error) {
-      console.error("Error in handleAddTask:", error);
     }
   };
+
 
   return (
     <Dialog open={isAddModalOpen} onOpenChange={handleAddModalClose}>
       <DialogContent>
+        
         <DialogHeader>
-          <DialogTitle>{newTask._id ? "Edit Task" : "Add New Task"}</DialogTitle>
+          <DialogTitle>
+            {newTask._id ? "Edit Task" : "Add New Task"}
+          </DialogTitle>
         </DialogHeader>
 
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
+        <div className="grid gap-4 py-4 ">
+          <div className="grid grid-cols-4 items-center gap-4 ">
             <Label htmlFor="title" className="text-left">
               Title
             </Label>
             <Input
               id="title"
               value={newTask.title}
-              onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+              onChange={(e) =>
+                setNewTask({ ...newTask, title: e.target.value })
+              }
               className="col-span-3"
             />
           </div>
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-left">
               Description
@@ -104,18 +113,21 @@ const AddTaskModal = () => {
             <Textarea
               id="description"
               value={newTask.description}
-              onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+              onChange={(e) =>
+                setNewTask({ ...newTask, description: e.target.value })
+              }
               className="col-span-3"
             />
           </div>
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="status" className="text-left">
               Status
             </Label>
             <Select
-              value={newTask.status || "To Do"}
-              onValueChange={(value) => setNewTask({ ...newTask, status: value as TaskStatus })}
+              value={newTask.status}
+              onValueChange={(value) =>
+                setNewTask({ ...newTask, status: value as TaskStatus })
+              }
             >
               <SelectTrigger className="col-span-3">
                 <SelectValue placeholder="Select status" />
@@ -127,14 +139,15 @@ const AddTaskModal = () => {
               </SelectContent>
             </Select>
           </div>
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="priority" className="text-left">
               Priority
             </Label>
             <Select
-              value={newTask.priority || "Urgent"}
-              onValueChange={(value) => setNewTask({ ...newTask, priority: value as TaskPriority })}
+              value={newTask.priority}
+              onValueChange={(value) =>
+                setNewTask({ ...newTask, priority: value as TaskPriority })
+              }
             >
               <SelectTrigger className="col-span-3">
                 <SelectValue placeholder="Select priority" />
@@ -146,7 +159,6 @@ const AddTaskModal = () => {
               </SelectContent>
             </Select>
           </div>
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="dueDate" className="text-left">
               Due Date
@@ -154,12 +166,11 @@ const AddTaskModal = () => {
             <Input
               id="dueDate"
               type="date"
-              value={newTask.dueDate ? format(newTask.dueDate, "yyyy-MM-dd") : ""}
+              value={
+                newTask.dueDate ? format(newTask.dueDate, "yyyy-MM-dd") : ""
+              }
               onChange={(e) =>
-                setNewTask({
-                  ...newTask,
-                  dueDate: e.target.value ? new Date(e.target.value) : undefined,
-                })
+                setNewTask({...newTask,dueDate: e.target.value? new Date(e.target.value): undefined,})
               }
               className="col-span-3 w-fit"
             />
@@ -171,6 +182,7 @@ const AddTaskModal = () => {
             {newTask._id ? "Save Changes" : "Add Task"}
           </Button>
         </DialogFooter>
+
       </DialogContent>
     </Dialog>
   );
